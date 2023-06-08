de8c8c45ec37bd51918e15501e41dbac5e0a07bc
Saadanaikar <135764640+Saadanaikar@users.noreply.github.com>
2023-06-08 17:32:39 -0400

    UT4-40 implemented react navigation to the project

--- Upscale-frontend-project/App.js
+++ Upscale-frontend-project/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './screens/HomeScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working </Text>
-      <StatusBar style="auto" /
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
