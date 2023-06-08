b2b86a23883b38963f1b3397bd74ab4440e8a137
Saadanaikar <135764640+Saadanaikar@users.noreply.github.com>
2023-06-08 17:00:31 -0400

    hello

--- Upscale-frontend-project/App.js
+++ Upscale-frontend-project/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working </Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /
     </View>
   );
 }
