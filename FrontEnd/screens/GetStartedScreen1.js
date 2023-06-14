5f76cb804ce48b64415ec027bf26bd538d864a16
NithinBheeshman <sriniv73@uwindsor.ca>
2023-06-13 20:59:45 -0500

    UT4-75 GetStartedPage created

---
+++ FrontEnd/screens/GetStartedScreen1.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GetStartedScreen1 = () => {
+  return (
+    <View>
+      <Text>GetStartedScreen1</Text>
+    </View>
+  )
+}
+
+export default GetStartedScreen1
+
+const styles = StyleSheet.create({})
