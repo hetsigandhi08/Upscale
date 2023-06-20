3f309ab5a51bce810cbdc2dc9da9ea4a69ae72a9
NithinBheeshman <sriniv73@uwindsor.ca>
2023-06-19 19:17:46 -0500

    UT4-75 GetStartedScreen1done

---
+++ FrontEnd/screens/GetStartesScreen2.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GetStartesScreen2 = () => {
+  return (
+    <View>
+      <Text>GetStartesScreen2</Text>
+    </View>
+  )
+}
+
+export default GetStartesScreen2
+
+const styles = StyleSheet.create({})
