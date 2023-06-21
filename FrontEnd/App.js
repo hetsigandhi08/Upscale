de1ad2a3d9e42a1cfb9d773f8436df54952047e2
NithinBheeshman <sriniv73@uwindsor.ca>
2023-06-21 10:55:26 -0500

    UT4-79 page 3 started

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -11,7 +11,7 @@ import OnBoardingScreen3 from './screens/OnBoardingScreen3';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import GetStartedScreen1 from './screens/GetStartedScreen1';
 import GetStartedScreen2 from './screens/GetStartedScreen2';
-
+import GetStartedScreen3 from './screens/GetStartedScreen3';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -32,9 +32,10 @@ export default function App() {
   }
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="GetStarted1">
+      <Stack.Navigator initialRouteName="GetStarted3">
       <Stack.Screen name = "GetStarted" options = {{headerShown: false}} component = {GetStartedScreen1} />
       <Stack.Screen name = "GetStarted2" options = {{headerShown: false}} component = {GetStartedScreen2} />
+      <Stack.Screen name = "GetStarted3" options = {{headerShown: false}} components = {GetStartedScreen3} />
       <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
       <Stack.Screen  name="Login" options={{headerShown: false}} component={LoginScreen} />
       <Stack.Screen  name="OnBoardingScreen1" options={{headerShown: false}} component={OnBoardingScreen1} />
