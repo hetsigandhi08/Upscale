42ecbc3c38b071ff289df10bf6a9f6d543da8f39
Haaris <62899376+harris-20@users.noreply.github.com>
2023-07-03 22:45:30 -0400

    forget password,Enter recovery mail, Enter security code

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,64 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useCallback } from 'react';
-import { useFonts } from 'expo-font';
-import BottomTabScreen from './screens/BottomTabScreen';
-import LoginScreen from './screens/LoginScreen'
-import HomeScreen from './screens/HomeScreen';
-import OnBoardingScreen1 from './screens/OnBoardingScreen1';
-import OnBoardingScreen2 from './screens/OnBoardingScreen2';
-import OnBoardingScreen3 from './screens/OnBoardingScreen3';
-import RoadMapScreen from './screens/RoadMapScreen';
-import CourseDetailsScreen from './screens/CourseDetailsScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import GetStartedScreen1 from './screens/GetStartedScreen1';
-import GetStartedScreen2 from './screens/GetStartedScreen2';
-import GetStartedScreen3 from './screens/GetStartedScreen3';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
+const RecoveryMailScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [emailFocusState, setEmailFocusState] = useState(false);
 
-const Stack = createNativeStackNavigator();
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    'SourceSans3-SemiBold': require('./assets/fonts/SourceSans3-SemiBold.ttf'),
-    'SourceSans3-Regular': require('./assets/fonts/SourceSans3-Regular.ttf'),
-    'SourceSans3-Bold': require('./assets/fonts/SourceSans3-Bold.ttf'),
-  });
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
+  const handleSendRecoveryMail = () => {
+    // Replace this with your actual logic for sending recovery mail
+    if (email.trim() === '') {
+      alert('Please enter a valid email address.');
+    } else {
+      // Code for sending recovery mail goes here
+      console.log(`Recovery mail sent to ${email}`);
+      alert('Recovery mail sent successfully!');
+      setEmail('');
     }
-  }, [fontsLoaded]);
+  };
 
-  if (!fontsLoaded) {
-    return null;
-  }
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="GetStarted3">
-      <Stack.Screen name = "GetStarted" options = {{headerShown: false}} component = {GetStartedScreen1} />
-      <Stack.Screen name = "GetStarted2" options = {{headerShown: false}} component = {GetStartedScreen2} />
-      <Stack.Screen name = "GetStarted3" options = {{headerShown: false}} components = {GetStartedScreen3} />
-      <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
-      <Stack.Screen name="Tab" options={{headerShown: false}} component={BottomTabScreen} />
-      <Stack.Screen  name="Login" options={{headerShown: false}} component={LoginScreen} />
-      <Stack.Screen name="RoadMap" options={{headerShown: false}} component={RoadMapScreen} />
-      <Stack.Screen  name="CourseDetails" options={{headerShown: false}} component={CourseDetailsScreen} />
-      <Stack.Screen  name="OnBoardingScreen1" options={{headerShown: false}} component={OnBoardingScreen1} />
-      <Stack.Screen  name="OnBoardingScreen2" options={{headerShown: false}} component={OnBoardingScreen2} />
-      <Stack.Screen  name="OnBoardingScreen3" options={{headerShown: false}} component={OnBoardingScreen3} />
-      </Stack.Navigator>
-      <StatusBar backgroundColor='#FFFFFF' style="dark"/>
-    </NavigationContainer>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <Text style={styles.title}>Enter Recovery mail</Text>
+          <Text style={styles.subTitle}>Enter the email to recover your account.</Text>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={emailFocusState ? styles.mailInputFocused : styles.mailInput}
+            placeholder='Email Address'
+            autoComplete='email'
+            onBlur={() => setEmailFocusState(false)}
+            onFocus={() => setEmailFocusState(true)}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <TouchableOpacity style={styles.sendMailBtn} onPress={handleSendRecoveryMail}>
+            <Text style={styles.sendMailText}>Next</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    </TouchableWithoutFeedback>
   );
-}
+};
+
+export default RecoveryMailScreen;
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#FAFCFB',
+    display: 'flex',
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center', // Center vertically and horizontally
+    padding: 35,
+  },
+  topContainer: {
+    width: '100%',
+    gap: 6,
+    alignItems: 'flex-start', // Align items to the left side
+  },
+  inputContainer: {
+    alignItems: 'center',
+    gap: 8,
+    marginTop: 45, // Add margin top
+  },
+  title: {
+    fontSize: 30,
+    fontFamily: 'SourceSans3-SemiBold',
+  },
+  subTitle: {
+    fontSize: 14,
+    fontWeight: '400',
+    fontFamily: 'SourceSans3-Regular',
+    color: '#636363',
+  },
+  mailInput: {
+    width: 320,
+    height: 46,
+    fontSize: 14,
+    fontFamily: 'SourceSans3-Regular',
+    paddingHorizontal: 20,
+    color: '#f0f8ff',
+    backgroundColor: '#FBFBFB',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#A9A9A9',
+  },
+
+  mailInputFocused: {
+    width: 320,
+    height: 46,
+    fontSize: 14,
+    fontFamily: 'SourceSans3-Regular',
+    paddingHorizontal: 20,
+    backgroundColor: '#FBFBFB',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#5237C0',
+  },
+  sendMailBtn: {
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    width: 263,
+    height: 47,
+    backgroundColor: '#00008b',
+    borderRadius: 42,
+    marginTop: 20,
+  },
+  sendMailText: {
+    color: '#FEFEFE',
+    fontFamily: 'SourceSans3-Regular',
   },
 });
--- FrontEnd/constant/Constant.js
+++
@@ -1,3 +0,0 @@
-const url = "http://192.168.2.16:3000/";
-
-export default url ;
